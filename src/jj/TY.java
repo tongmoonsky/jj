d8e55176c019fcf949b6bc4fd7a834df756d6f7b
TONGMOON\彤彤 <986098090@qq.com>
2018-03-21 21:16:29 +0800

    haha

--- src/jj/TY.java
+++ src/jj/TY.java
@@ -1,9 +1,17 @@
 package jj;
-
+/***
+ * 
+ * @author 彤彤
+ *
+ */
 public class TY {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("明天你好");
+		System.out.print("明天你好");
+		System.out.print("明天你好");
+		System.out.println("明天你好");
 
 	}
 
